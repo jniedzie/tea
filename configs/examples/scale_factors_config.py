659a3b04a8d6e796143dddc8729e803a0a0e76ea
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2024-01-09 10:57:11 +0100

    Cleaning up scale factors config.

--- configs/examples/scale_factors_config.py
+++ configs/examples/scale_factors_config.py
@@ -1,37 +1,6 @@
-from ScaleFactorsReader import ScaleFactorsReader
-from Logger import *
-
-import os
-import urllib.request
-import gzip
-
-scaleFactorsReader = ScaleFactorsReader()
-
-muonSFs = {
-  # Reco SFs
-  **scaleFactorsReader.getMuonScaleFactors("../tea/data/muon_SFs/muon_highPt_recoID.json"),
-  **scaleFactorsReader.getMuonScaleFactors("../tea/data/muon_SFs/NUM_TrackerMuons_DEN_genTracks_Z_abseta_pt.json"),
-  **scaleFactorsReader.getMuonScaleFactors("../tea/data/muon_SFs/Efficiency_muon_generalTracks_Run2018_UL_trackerMuon.json"),
-
-  # ID SFs
-  **scaleFactorsReader.getMuonScaleFactors("../tea/data/muon_SFs/Efficiency_muon_trackerMuon_Run2018_UL_ID.json"),
-  **scaleFactorsReader.getMuonScaleFactors("../tea/data/muon_SFs/Efficiencies_muon_generalTracks_Z_Run2018_UL_ID.json"),
-
-  # Iso SFs
-  **scaleFactorsReader.getMuonScaleFactors("../tea/data/muon_SFs/Efficiencies_muon_generalTracks_Z_Run2018_UL_ISO.json"),
-
-  # Trigger SFs
-  **scaleFactorsReader.getMuonTriggerScaleFactors("../tea/data/muon_SFs/Efficiencies_muon_generalTracks_Z_Run2018_UL_SingleMuonTriggers_schemaV2.json"),
-}
-
-bTaggingSFs = {
-  # B-tagging SFs
-  **scaleFactorsReader.getBtaggingScaleFactors("../tea/data/b_tagging/btagging_UL2018.json"),
-}
-
-muonSFsPath = "../tea/jsonPOG/POG/MUO/2018_UL/muon_Z.json.gz"
 
 scaleFactors = {
+  # b-tagging
   "bTaggingMedium": {
     "path": "../tea/jsonPOG/POG/BTV/2018_UL/btagging.json.gz",
     "type": "deepJet_mujets",
@@ -48,6 +17,7 @@ scaleFactors = {
     "jetID": "5",
   },
   
+  #  Muon Reco
   "muonReco": {
     "path": "../tea/jsonPOG/POG/MUO/2018_UL/muon_Z.json.gz",
     "type": "NUM_TrackerMuons_DEN_genTracks",
@@ -55,6 +25,7 @@ scaleFactors = {
     "ValType": "sf",
   },
   
+  # Muon ID
   "muonIDLoose": {
     "path": "../tea/jsonPOG/POG/MUO/2018_UL/muon_Z.json.gz",
     "type": "NUM_LooseID_DEN_TrackerMuons", 
@@ -74,6 +45,7 @@ scaleFactors = {
     "ValType": "sf",
   },
   
+  # Muon Iso
   "muonIsoLoose": {
     "path": "../tea/jsonPOG/POG/MUO/2018_UL/muon_Z.json.gz",
     "type": "NUM_LooseRelIso_DEN_LooseID",
@@ -87,6 +59,7 @@ scaleFactors = {
     "ValType": "sf",
   },
   
+  # Muon trigger
   "muonTriggerIsoMu24": {
     "path": "../tea/jsonPOG/POG/MUO/2018_UL/muon_Z.json.gz",
     "type": "NUM_IsoMu24_DEN_CutBasedIdTight_and_PFIsoTight",
@@ -94,5 +67,11 @@ scaleFactors = {
     "ValType": "sf",
   },
   
-  
+  # Pileup
+  # "pileup": {
+  #   "path": "../tea/jsonPOG/POG/LUM/2018_UL/puWeights.json.gz",
+  #   "type": "NUM_IsoMu24_DEN_CutBasedIdTight_and_PFIsoTight",
+  #   "year": "2018_UL",
+  #   "ValType": "sf",
+  # },
 }
