03abd1efb9d432132d62b4eeaa9fb815fb65406c
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2024-06-03 17:26:15 +0200

    Returning nullptr when converting to different object type if it was nullptr to start with.

--- libs/extensions/include/ExtensionsHelpers.hpp
+++ libs/extensions/include/ExtensionsHelpers.hpp
@@ -12,26 +12,33 @@
 #include "PhysicsObject.hpp"
 
 inline std::shared_ptr<NanoGenParticle> asNanoGenParticle(const std::shared_ptr<PhysicsObject> physicsObject) {
+  if(!physicsObject) return nullptr;
   return std::make_shared<NanoGenParticle>(physicsObject);
 }
 
 inline std::shared_ptr<NanoMuon> asNanoMuon(const std::shared_ptr<PhysicsObject> physicsObject) {
+  if(!physicsObject) return nullptr;
   return std::make_shared<NanoMuon>(physicsObject);
 }
 
 inline std::shared_ptr<NanoDimuonVertex> asNanoDimuonVertex(const std::shared_ptr<PhysicsObject> physicsObject, const std::shared_ptr<Event> event) {
+  if(!physicsObject || !event) return nullptr;
   return std::make_shared<NanoDimuonVertex>(physicsObject, event);
 }
 
 inline std::shared_ptr<NanoElectron> asNanoElectron(const std::shared_ptr<PhysicsObject> physicsObject) {
+  if(!physicsObject) return nullptr;
   return std::make_shared<NanoElectron>(physicsObject);
 }
 
 inline std::shared_ptr<NanoJet> asNanoJet(const std::shared_ptr<PhysicsObject> physicsObject) {
+  if(!physicsObject) return nullptr;
   return std::make_shared<NanoJet>(physicsObject);
 }
 
 inline std::shared_ptr<HepMCParticle> asHepMCParticle(const std::shared_ptr<PhysicsObject> physicsObject) {
+  if(!physicsObject) return nullptr;
+  
   int index = physicsObject->GetIndex();
   if (index < 0) {
     fatal() << "Error in asHepMCParticle(...). Make sure to set index of the physics object using SetIndex()." << std::endl;
@@ -41,6 +48,7 @@ inline std::shared_ptr<HepMCParticle> asHepMCParticle(const std::shared_ptr<Phys
 }
 
 inline std::shared_ptr<NanoEvent> asNanoEvent(const std::shared_ptr<Event> physicsObject) {
+  if(!physicsObject) return nullptr;
   return std::make_shared<NanoEvent>(physicsObject);
 }
 
