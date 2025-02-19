cc546b18b450fa6f44eda126b1b8c32353af0d0a
Lovisa <lovisa.rygaard@cern.ch>
2025-02-19 14:04:54 +0100

    Golden json for 2022 and 2023

--- configs/examples/golden_json_config.py
+++ configs/examples/golden_json_config.py
@@ -1,6 +1,8 @@
 import json
 
 input_path = "../tea/data/golden_json/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt"
+# input_path = "../tea/data/golden_json/Cert_Collisions2022_355100_362760_Golden.txt"
+# input_path = "../tea/data/golden_json/Cert_Collisions2023_366442_370790_Golden.txt"
 
 # read JSON file into a dictionary
 with open(input_path) as f:
