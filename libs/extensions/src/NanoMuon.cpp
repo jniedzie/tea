d28adf1b45c3135149ba76a3f12a8f5e7e66b27f
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2025-03-21 11:46:39 +0100

    Adding a method to find gen muon closest to a NanoMuon

--- libs/extensions/src/NanoMuon.cpp
+++ libs/extensions/src/NanoMuon.cpp
@@ -1,5 +1,6 @@
 #include "NanoMuon.hpp"
 #include "ConfigManager.hpp"
+#include "ExtensionsHelpers.hpp"
 
 using namespace std;
 
@@ -62,6 +63,31 @@ int NanoMuon::GetMatchesForNthBestMatch(int N) {
  if (isDSA()) matchString = "muonMatch" + to_string(N);
  if (!isDSA()) matchString = "dsaMatch" + to_string(N);
  return GetAs<int>(matchString);
+}
+
+shared_ptr<NanoGenParticle> NanoMuon::GetGenMuon(shared_ptr<PhysicsObjects> genParticles, float maxDeltaR){
+  shared_ptr<NanoGenParticle> bestGenMuon = nullptr;
+  float bestDeltaR = maxDeltaR;
+
+  float eta = GetEta();
+  float phi = GetPhi();
+
+  for(auto physObj : *genParticles){
+    auto genParticle = asNanoGenParticle(physObj);
+    if(!genParticle->IsMuon()) continue;
+
+    float genEta = genParticle->Get("eta");
+    float genPhi = genParticle->Get("phi");  
+    float dEta = eta - genEta;
+    float dPhi = TVector2::Phi_mpi_pi(phi - genPhi);
+    float deltaR = TMath::Sqrt(dEta*dEta + dPhi*dPhi);
+    if(deltaR < bestDeltaR && deltaR < maxDeltaR){
+      bestDeltaR = deltaR;
+      bestGenMuon = genParticle;
+    }
+  }
+  return bestGenMuon;
+
 }
 
 float NanoMuon::OuterDeltaRtoMuon(shared_ptr<NanoMuon> muon) {
