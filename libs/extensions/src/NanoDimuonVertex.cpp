0f508ed60b648f81d23f32a4ef9af804c7d57e51
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2025-04-07 16:05:46 +0200

    Making the code a little bit more compact

--- libs/extensions/src/NanoDimuonVertex.cpp
+++ libs/extensions/src/NanoDimuonVertex.cpp
@@ -171,15 +171,9 @@ int NanoDimuonVertex::GetTotalNumberOfCSCHits() {
 }
 
 shared_ptr<NanoMuon> NanoDimuonVertex::GetLeadingMuon() {
-  if (muon1->GetAs<float>("pt") > muon2->GetAs<float>("pt"))
-    return muon1;
-  else
-    return muon2;
+  return (muon1->GetAs<float>("pt") > muon2->GetAs<float>("pt")) ? muon1 : muon2;
 }
 
 shared_ptr<NanoMuon> NanoDimuonVertex::GetSubleadingMuon() {
-  if (muon1->GetAs<float>("pt") < muon2->GetAs<float>("pt"))
-    return muon1;
-  else
-    return muon2;
+  return (muon1->GetAs<float>("pt") < muon2->GetAs<float>("pt")) ? muon1 : muon2;
 }
