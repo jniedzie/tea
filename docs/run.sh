b23e471f6be2acedd79fab6f8505cefa500f9dc2
jniedzie <jeremi.niedziela@cern.ch>
2025-06-16 16:22:47 +0200

    Improving setup to check for changes automatically.

--- docs/run.sh
+++ docs/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # docker build --platform=linux/arm64 -f Dockerfile.jekyll -t my-jekyll .
-docker run --rm -p 4000:4000 my-jekyll
+docker run --rm \
+  -p 4000:4000 \
+  -p 35729:35729 \
+  -v "$PWD:/srv/jekyll" \
+  my-jekyll
+
