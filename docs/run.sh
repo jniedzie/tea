c9bc6136415eb2b99e67c74a6574a31fffa5a54e
jniedzie <jeremi.niedziela@cern.ch>
2025-06-17 17:42:42 +0200

    Rebuilding by default.

--- docs/run.sh
+++ docs/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# docker build --platform=linux/arm64 -f Dockerfile.jekyll -t my-jekyll .
+docker build --platform=linux/arm64 -f Dockerfile.jekyll -t my-jekyll .
 docker run --rm \
   -p 4000:4000 \
   -p 35729:35729 \
