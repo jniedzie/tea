a747f34d8bd7a0cbb5d45f6bbd03e5bf463acd8e
Lovisa <lovisa.rygaard@cern.ch>
2025-06-23 16:13:36 +0200

    Fixing memory problem

--- libs/core/include/ScaleFactorsManager.hpp
+++ libs/core/include/ScaleFactorsManager.hpp
@@ -21,8 +21,8 @@ struct MuonIso;
 class ScaleFactorsManager {
  public:
   static ScaleFactorsManager &GetInstance() {
-    static ScaleFactorsManager instance;
-    return instance;
+    static ScaleFactorsManager* instance = new ScaleFactorsManager();
+    return *instance;
   }
 
   ScaleFactorsManager(ScaleFactorsManager const &) = delete;
@@ -51,15 +51,10 @@ class ScaleFactorsManager {
   }
   std::map<std::string, std::vector<bool>> applyScaleFactors;
 
-  CorrectionRef bTaggingCorrections;
-  CorrectionRef muonCorrections;
-
   std::map<std::string, CorrectionRef> corrections;
   std::map<std::string, std::map<std::string, std::string>> correctionsExtraArgs;
 
-  std::map<std::string, TH2D *> muonSFvalues;
   TH1D *pileupSFvalues;
-  std::map<std::string, TF1 *> btaggingSFvalues;
 
   bool ShouldApplyScaleFactor(const std::string &name);
   bool ShouldApplyVariation(const std::string &name);
