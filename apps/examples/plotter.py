0f1a88c646f01a17738c47400010687410fea606
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2025-08-22 15:17:45 +0200

    Additional protection in the plotter.

--- apps/examples/plotter.py
+++ apps/examples/plotter.py
@@ -1,5 +1,5 @@
 from HistogramPlotter import HistogramPlotter
-from Logger import *
+from Logger import error, logger_print
 
 import ROOT
 import importlib
@@ -21,34 +21,38 @@ def main():
   plotter = HistogramPlotter(config)
 
   input_files = {}
-  
+
   for sample in config.samples:
     try:
       input_files[sample.name] = ROOT.TFile.Open(sample.file_path, "READ")
     except OSError:
       error(f"File {sample.file_path} not found!")
       continue
-    
+
     for hist in config.histograms:
       plotter.addHistosample(hist, sample, input_files[sample.name])
-    
-    if hasattr(config, "histograms2D"):    
+
+    if hasattr(config, "histograms2D"):
       for hist in config.histograms2D:
         plotter.addHistosample2D(hist, sample, input_files[sample.name])
 
-    if hasattr(config, "histogramsRatio"):    
+    if hasattr(config, "histogramsRatio"):
       for histpair in config.histogramsRatio:
-        plotter.addHistosampleRatio(histpair[0], histpair[1], sample, input_files[sample.name])
+        plotter.addHistosampleRatio(
+          histpair[0], histpair[1], sample, input_files[sample.name]
+        )
 
   plotter.setupLegends()
   plotter.buildStacks()
   plotter.buildStacksRatio()
-  plotter.addHists2D(input_files[sample.name], sample)
+  if sample.name in input_files:
+    plotter.addHists2D(input_files[sample.name], sample)
   plotter.drawStacks()
   plotter.drawRatioStacks()
   plotter.drawHists2D()
-  
+
   logger_print()
+
 
 if __name__ == "__main__":
   main()
