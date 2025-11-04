d405b4cb79db4f62bfe34b0d4fb7b2d58ce0e331
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2025-11-04 09:11:52 +0100

    Starting jet veto maps implementation

--- libs/core/src/Event.cpp
+++ libs/core/src/Event.cpp
@@ -147,6 +147,68 @@ bool Event::PassesHEMveto(float affectedFraction) {
   return true;
 }
 
+bool Event::PassesJetVetoMaps(float affectedFraction) {
+  // Implemented based on the recommendations from:
+  // https://cms-jerc.web.cern.ch/Recommendations/#jet-veto-maps
+
+  if (config.GetYear() != "2018") return true;  // HEM veto only applies to 2018 data/MC
+
+  if (!IsData()) {
+    float randNum = randFloat();
+    if (randNum > affectedFraction) {
+      return true;
+    }
+  } else {
+    unsigned runNumber = Get("run");
+    if (runNumber < 319077) {
+      warn() << "Run number less than 319077 found in 2018 data/MC. HEM veto will not be applied for this event." << endl;
+      return true;  // HEM veto only applies to runs >= 319077
+    }
+  }
+
+  auto jets = GetCollection("Jet");
+  auto muons = GetCollection("Muon");
+
+  for (auto& jet : *jets) {
+    // jet pT > 15 GeV
+    float jetPt = jet->Get("pt");
+    if (jetPt < 15) continue;
+
+    // tight jet ID with lep veto OR [tight jet ID & (jet EM fraction < 0.9) & (jets that don’t overlap with PF muon (dR < 0.2)]
+    int jetID = jet->Get("jetId");
+
+    bool overlapsWithMuon = false;
+    auto jetVector = jet->GetFourVector();
+
+    for (auto& muon : *muons) {
+      auto muonVector = muon->GetFourVector();
+      float dR = jetVector.DeltaR(muonVector);
+      if (dR < 0.2) {
+        overlapsWithMuon = true;
+        break;
+      }
+    }
+    float jetEmEF = jet->GetAs<float>("chEmEF") + jet->GetAs<float>("neEmEF");
+
+    // bit1 is loose (always false in 2017 since it does not exist), bit2 is tight, bit3 is tightLepVeto*
+    bool passesID = (jetID & 0b100) || ((jetID & 0b010) && (jetEmEF < 0.9) && !overlapsWithMuon);
+    if (!passesID) continue;
+
+    // PU jet ID for AK4chs jets with pT < 50 GeV (No PUjetID required for PUPPI jets)
+    int jetPUid = jet->Get("puId");
+    if (jetPt < 50 && jetPUid == 0) continue;
+
+    // check if jet is in HEM region
+    // jets with -1.57 <phi< -0.87 and -2.5<eta<-1.3
+    // jets with -1.57 <phi< -0.87 and -3.0<eta<-2.5
+
+    if (jetVector.Eta() >= -3.0 && jetVector.Eta() <= -1.3 && jetVector.Phi() >= -1.57 && jetVector.Phi() <= -0.87) {
+      return false;
+    }
+  }
+  return true;
+}
+
 bool Event::IsData() {
   // Test 1: gen weights branch only for MC
   bool isData = false;
