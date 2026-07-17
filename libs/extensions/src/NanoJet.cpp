8852b0c134da4b24d1e91c6c48594df64dfbacd9
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2026-07-17 11:46:32 +0200

    Fixing gcc issues on lxplus after correctionlib updates

--- libs/extensions/src/NanoJet.cpp
+++ libs/extensions/src/NanoJet.cpp
@@ -1,4 +1,5 @@
 #include "NanoJet.hpp"
+
 #include "ConfigManager.hpp"
 #include "ScaleFactorsManager.hpp"
 
@@ -12,51 +13,53 @@ TLorentzVector NanoJet::GetFourVector() {
   return v;
 }
 
-map<string,float> NanoJet::GetBtaggingScaleFactors(string workingPoint) {
-  auto &scaleFactorsManager = ScaleFactorsManager::GetInstance();
+map<string, float> NanoJet::GetBtaggingScaleFactors(string workingPoint) {
+  auto& scaleFactorsManager = ScaleFactorsManager::GetInstance();
   return scaleFactorsManager.GetBTagScaleFactors(workingPoint, GetAbsEta(), GetPt());
 }
 
-map<string,float> NanoJet::GetPUJetIDScaleFactors(string name) {
-  auto &scaleFactorsManager = ScaleFactorsManager::GetInstance();
+map<string, float> NanoJet::GetPUJetIDScaleFactors(string name) {
+  auto& scaleFactorsManager = ScaleFactorsManager::GetInstance();
   return scaleFactorsManager.GetPUJetIDScaleFactors(name, GetEta(), GetPt());
 }
 
-map<string,float> NanoJet::GetJetEnergyCorrections(float rho) {
-  auto &scaleFactorsManager = ScaleFactorsManager::GetInstance();
+map<string, float> NanoJet::GetJetEnergyCorrections(float rho) {
+  auto& scaleFactorsManager = ScaleFactorsManager::GetInstance();
 
   float pt = GetPt();
   float rawFactor = Get("rawFactor");
 
-  map<string, float> inputs  = {{"Rho", rho}};
+  map<string, float> inputs = {{"Rho", rho}};
   inputs["JetA"] = (float)physicsObject->Get("area");
   inputs["JetEta"] = (float)physicsObject->Get("eta");
   inputs["JetPt"] = pt * (1 - rawFactor);
   inputs["JetMass"] = (float)physicsObject->Get("mass");
   inputs["JetPhi"] = (float)physicsObject->Get("phi");
-  map<string,float> corrections = scaleFactorsManager.GetJetEnergyCorrections(inputs);
+  map<string, float> corrections = scaleFactorsManager.GetJetEnergyCorrections(inputs);
   return corrections;
 }
 
 void NanoJet::AddSmearedPtByResolution(float rho, int eventID, shared_ptr<NanoEvent> event) {
-  auto &scaleFactorsManager = ScaleFactorsManager::GetInstance();
+  auto& scaleFactorsManager = ScaleFactorsManager::GetInstance();
   float pt = GetPt();
 
   // ScaleFactor
-  map<string,float> jerSF = scaleFactorsManager.GetJetEnergyResolutionScaleFactorAndPtResolution((float)physicsObject->Get("eta"), GetPt(), rho);
+  map<string, float> jerSF =
+      scaleFactorsManager.GetJetEnergyResolutionScaleFactorAndPtResolution((float)physicsObject->Get("eta"), GetPt(), rho);
 
   float genPt = -1;
   auto genJet = GetGenJetAsRecommendedForJER(event, jerSF["PtResolution"]);
-  if (genJet)
-    genPt = genJet->Get("pt");
+  if (genJet) genPt = genJet->Get("pt");
 
-  map<string, CorrectionArgType> inputs  = {{"JetPt", GetPt()}};
-  inputs["JetEta"] = (float)physicsObject->Get("eta");
-  inputs["GenPt"] = (double)genPt;
-  inputs["Rho"] = (double)rho;
-  inputs["EventID"] = (long)eventID;
-  inputs["JER"] = (double)jerSF["PtResolution"];
-  inputs["JERSF"] = (double)jerSF["systematic"];
+  map<string, CorrectionArgType> inputs = {
+      {"JetPt", GetPt()},
+      {"JetEta", (float)physicsObject->Get("eta")},
+      {"GenPt", (double)genPt},
+      {"Rho", (double)rho},
+      {"EventID", (long)eventID},
+      {"JER", (double)jerSF["PtResolution"]},
+      {"JERSF", (double)jerSF["systematic"]},
+  };
 
   float jetPt_factor = scaleFactorsManager.GetJetEnergyResolutionSmearingFactor(inputs);
   inputs["JERSF"] = (double)jerSF["jerMC_ScaleFactor_up"];
@@ -64,12 +67,12 @@ void NanoJet::AddSmearedPtByResolution(float rho, int eventID, shared_ptr<NanoEv
   inputs["JERSF"] = (double)jerSF["jerMC_ScaleFactor_down"];
   float jetPt_factor_down = scaleFactorsManager.GetJetEnergyResolutionSmearingFactor(inputs);
 
-  physicsObject->SetFloat("pt_smeared" , GetPt()*jetPt_factor);
-  physicsObject->SetFloat("pt_smeared_up" , GetPt()*jetPt_factor_up);
-  physicsObject->SetFloat("pt_smeared_down" , GetPt()*jetPt_factor_down);
-  physicsObject->SetFloat("mass_smeared" , GetMass()*jetPt_factor);
-  physicsObject->SetFloat("mass_smeared_up" , GetMass()*jetPt_factor_up);
-  physicsObject->SetFloat("mass_smeared_down" , GetMass()*jetPt_factor_down);
+  physicsObject->SetFloat("pt_smeared", GetPt() * jetPt_factor);
+  physicsObject->SetFloat("pt_smeared_up", GetPt() * jetPt_factor_up);
+  physicsObject->SetFloat("pt_smeared_down", GetPt() * jetPt_factor_down);
+  physicsObject->SetFloat("mass_smeared", GetMass() * jetPt_factor);
+  physicsObject->SetFloat("mass_smeared_up", GetMass() * jetPt_factor_up);
+  physicsObject->SetFloat("mass_smeared_down", GetMass() * jetPt_factor_down);
 }
 
 shared_ptr<PhysicsObject> NanoJet::GetGenJetAsRecommendedForJER(shared_ptr<NanoEvent> event, float sigma_JER, float R_cone) {
@@ -80,12 +83,10 @@ shared_ptr<PhysicsObject> NanoJet::GetGenJetAsRecommendedForJER(shared_ptr<NanoE
     float dEta = eta - float(genJet->Get("eta"));
     float dPhi = TVector2::Phi_mpi_pi(phi - float(genJet->Get("phi")));
     float dR = TMath::Sqrt(dEta * dEta + dPhi * dPhi);
-    if (dR >= R_cone / 2) 
-      continue;
+    if (dR >= R_cone / 2) continue;
 
     float absDPt = fabs(GetPt() - (float)genJet->Get("pt"));
-    if (absDPt >= 3 * sigma_JER * GetPt())
-      continue;
+    if (absDPt >= 3 * sigma_JER * GetPt()) continue;
 
     return genJet;
   }
@@ -94,16 +95,14 @@ shared_ptr<PhysicsObject> NanoJet::GetGenJetAsRecommendedForJER(shared_ptr<NanoE
 
 float NanoJet::GetPxDifference(float newJetPt, float oldJetPt) {
   float phi = GetPhi();
-  if (oldJetPt < 0)
-    oldJetPt = GetPt();
+  if (oldJetPt < 0) oldJetPt = GetPt();
   float deltaPt = newJetPt - oldJetPt;
   return deltaPt * cos(phi);
 }
 
 float NanoJet::GetPyDifference(float newJetPt, float oldJetPt) {
   float phi = GetPhi();
-  if (oldJetPt < 0)
-    oldJetPt = GetPt();
+  if (oldJetPt < 0) oldJetPt = GetPt();
   float deltaPt = newJetPt - oldJetPt;
   return deltaPt * sin(phi);
 }
