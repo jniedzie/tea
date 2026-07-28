3d5417906e498d2546e82c04a4dada7ebcc99536
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2026-07-28 09:34:42 +0200

    Add scalar UShort_t branch support

--- libs/core/src/EventReader.cpp
+++ libs/core/src/EventReader.cpp
@@ -246,6 +246,9 @@ void EventReader::SetupScalarBranch(string branchName, string branchType, string
   } else if (branchType == "Short_t") {
     currentEvent->valuesShort[branchName] = 0;
     inputTrees[eventsTreeName]->SetBranchAddress(branchName.c_str(), &currentEvent->valuesShort[branchName]);
+  } else if (branchType == "UShort_t") {
+    currentEvent->valuesUshort[branchName] = 0;
+    inputTrees[eventsTreeName]->SetBranchAddress(branchName.c_str(), &currentEvent->valuesUshort[branchName]);
   } else {
     error() << "unsupported scalar branch type: " << branchType << "\t (branch name: " << branchName << ")" << endl;
   }
