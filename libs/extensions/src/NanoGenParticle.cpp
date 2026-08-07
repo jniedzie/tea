dff5651d9c0612c597917565489ee3507767a1c0
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2026-08-07 14:56:56 +0200

    Adding a method to check if a gen particle comes from a J/Ψ

--- libs/extensions/src/NanoGenParticle.cpp
+++ libs/extensions/src/NanoGenParticle.cpp
@@ -3,6 +3,8 @@
 //  Created by Jeremi Niedziela on 08/08/2023.
 
 #include "NanoGenParticle.hpp"
+#include <iomanip>
+#include <iostream>
 
 using namespace std;
 
@@ -76,8 +78,13 @@ shared_ptr<NanoGenParticle> NanoGenParticle::GetFirstCopy(shared_ptr<PhysicsObje
   return firstCopy;
 }
 
-#include <iostream>
-#include <iomanip>
+bool NanoGenParticle::IsMotherJPsi(const shared_ptr<PhysicsObjects> genParticles) {
+  Short_t motherIndex = Get("genPartIdxMother");
+  if (motherIndex < 0) return false;
+  auto mother = genParticles->at(motherIndex);
+  int motherPdgId = mother->Get("pdgId");
+  return (motherPdgId == 443);
+}
 
 void NanoGenParticle::Print() {
     const string cyan = "\033[36m";
