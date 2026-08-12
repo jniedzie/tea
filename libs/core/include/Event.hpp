ad132c94662124d8320e001834fbdd95691bd4bb
twybo <tiepolo.wybouw@gmail.com>
2026-08-12 10:51:04 +0200

    formatting and small PR comments fixed

--- libs/core/include/Event.hpp
+++ libs/core/include/Event.hpp
@@ -12,19 +12,25 @@
 #include "PhysicsObject.hpp"
 
 class Event {
- public:
+public:
   Event();
   ~Event();
 
   void Reset();
 
-  inline auto Get(std::string branchName, const char* file = __builtin_FILE(), const char* function = __builtin_FUNCTION(),
+  inline auto Get(std::string branchName, const char *file = __builtin_FILE(),
+                  const char *function = __builtin_FUNCTION(),
                   int line = __builtin_LINE()) {
-    if (valuesTypes.count(branchName) == 0 && customValuesTypes.count(branchName) == 0) {
-      std::string message = "\nTrying to access incorrect event-level branch: " + branchName;
-      if (branchName.find("Weight") != std::string::npos || branchName.find("Wgt") != std::string::npos ||
-          branchName.find("weight") != std::string::npos || branchName.find("wgt") != std::string::npos) {
-        message += ", it's probably fine for data if this is a gen weight branch.";
+    if (valuesTypes.count(branchName) == 0 &&
+        customValuesTypes.count(branchName) == 0) {
+      std::string message =
+          "\nTrying to access incorrect event-level branch: " + branchName;
+      if (branchName.find("Weight") != std::string::npos ||
+          branchName.find("Wgt") != std::string::npos ||
+          branchName.find("weight") != std::string::npos ||
+          branchName.find("wgt") != std::string::npos) {
+        message +=
+            ", it's probably fine for data if this is a gen weight branch.";
         warn() << message << std::endl;
       } else
         fatal(file, function, line) << message << std::endl;
@@ -34,8 +40,7 @@ class Event {
     return Multitype(this, branchName);
   }
 
-  template <typename T>
-  T GetAs(std::string branchName) {
+  template <typename T> T GetAs(std::string branchName) {
     if (defaultCollectionsTypes.count(branchName)) {
       std::string branchType = defaultCollectionsTypes[branchName];
       if (branchType == "Int_t") {
@@ -76,43 +81,44 @@ class Event {
       Float_t value = Get(branchName);
       defaultCollectionsTypes[branchName] = "Float_t";
       return value;
-    } catch (BadTypeException& e) {
+    } catch (BadTypeException &e) {
       try {
         Double_t value = Get(branchName);
         defaultCollectionsTypes[branchName] = "Double_t";
         return value;
-      } catch (BadTypeException& e) {
+      } catch (BadTypeException &e) {
         try {
           Int_t value = Get(branchName);
           defaultCollectionsTypes[branchName] = "Int_t";
           return value;
-        } catch (BadTypeException& e) {
+        } catch (BadTypeException &e) {
           try {
             UChar_t value = Get(branchName);
             defaultCollectionsTypes[branchName] = "UChar_t";
             return value;
-          } catch (BadTypeException& e) {
+          } catch (BadTypeException &e) {
             try {
               UShort_t value = Get(branchName);
               defaultCollectionsTypes[branchName] = "UShort_t";
               return value;
-            } catch (BadTypeException& e) {
+            } catch (BadTypeException &e) {
               try {
                 Short_t value = Get(branchName);
                 defaultCollectionsTypes[branchName] = "Short_t";
                 return value;
-              } catch (BadTypeException& e) {
+              } catch (BadTypeException &e) {
                 try {
                   UInt_t value = Get(branchName);
                   defaultCollectionsTypes[branchName] = "UInt_t";
                   return value;
-                } catch (BadTypeException& e) {
+                } catch (BadTypeException &e) {
                   try {
                     Bool_t value = Get(branchName);
                     defaultCollectionsTypes[branchName] = "Bool_t";
                     return value;
-                  } catch (BadTypeException& e) {
-                    error() << "Couldn't get value for branch " << branchName << std::endl;
+                  } catch (BadTypeException &e) {
+                    error() << "Couldn't get value for branch " << branchName
+                            << std::endl;
                   }
                 }
               }
@@ -125,30 +131,60 @@ class Event {
   }
 
   inline std::shared_ptr<PhysicsObjects> GetCollection(std::string name) const {
-    if (collections.count(name)) return collections.at(name);
-    if (extraCollections.count(name)) return extraCollections.at(name);
-    std::string message = "Tried to get a collection that doesn't exist: " + name;
+    if (collections.count(name))
+      return collections.at(name);
+    if (extraCollections.count(name))
+      return extraCollections.at(name);
+    std::string message =
+        "Tried to get a collection that doesn't exist: " + name;
     throw Exception(message.c_str());
   }
 
   void AddExtraCollections();
-  void AddCollection(std::string name, std::shared_ptr<PhysicsObjects> collection) { extraCollections.insert({name, collection}); }
-  void ReplaceCollection(std::string name, std::shared_ptr<PhysicsObjects> collection) { extraCollections[name] = collection; }
+  void AddCollection(std::string name,
+                     std::shared_ptr<PhysicsObjects> collection) {
+    extraCollections.insert({name, collection});
+  }
+  void ReplaceCollection(std::string name,
+                         std::shared_ptr<PhysicsObjects> collection) {
+    extraCollections[name] = collection;
+  }
 
-  const insertion_ordered_map<std::string, ExtraCollection>& GetExtraCollectionsDescriptions() const {
+  const insertion_ordered_map<std::string, ExtraCollection> &
+  GetExtraCollectionsDescriptions() const {
     return extraCollectionsDescriptions;
   }
 
-  Int_t* GetIntVector(std::string branchName) { return valuesIntVector.at(branchName); }
-  Bool_t* GetBoolVector(std::string branchName) { return valuesBoolVector.at(branchName); }
-  Float_t* GetFloatVector(std::string branchName) { return valuesFloatVector.at(branchName); }
-  Double_t* GetDoubleVector(std::string branchName) { return valuesDoubleVector.at(branchName); }
-  UChar_t* GetUcharVector(std::string branchName) { return valuesUcharVector.at(branchName); }
-  Char_t* GetCharVector(std::string branchName) { return valuesCharVector.at(branchName); }
-  UInt_t* GetUintVector(std::string branchName) { return valuesUintVector.at(branchName); }
-  UShort_t* GetUshortVector(std::string branchName) { return valuesUshortVector.at(branchName); }
-  Short_t* GetShortVector(std::string branchName) { return valuesShortVector.at(branchName); }
-  std::vector<unsigned int>* GetStdUintVector(std::string branchName) { return valuesStdUintVector.at(branchName); }
+  Int_t *GetIntVector(std::string branchName) {
+    return valuesIntVector.at(branchName);
+  }
+  Bool_t *GetBoolVector(std::string branchName) {
+    return valuesBoolVector.at(branchName);
+  }
+  Float_t *GetFloatVector(std::string branchName) {
+    return valuesFloatVector.at(branchName);
+  }
+  Double_t *GetDoubleVector(std::string branchName) {
+    return valuesDoubleVector.at(branchName);
+  }
+  UChar_t *GetUcharVector(std::string branchName) {
+    return valuesUcharVector.at(branchName);
+  }
+  Char_t *GetCharVector(std::string branchName) {
+    return valuesCharVector.at(branchName);
+  }
+  UInt_t *GetUintVector(std::string branchName) {
+    return valuesUintVector.at(branchName);
+  }
+  UShort_t *GetUshortVector(std::string branchName) {
+    return valuesUshortVector.at(branchName);
+  }
+  Short_t *GetShortVector(std::string branchName) {
+    return valuesShortVector.at(branchName);
+  }
+  std::vector<unsigned int> *GetStdUintVector(std::string branchName) {
+    return valuesStdUintVector.at(branchName);
+  }
 
   void SetFloat(std::string branchName, float value) {
     customValuesFloat[branchName] = value;
@@ -201,50 +237,60 @@ class Event {
     return it != customValuesEpoch.end() && it->second == gCustomValueEpoch;
   }
 
- private:
-  ConfigManager& config = ConfigManager::GetInstance();
+private:
+  ConfigManager &config = ConfigManager::GetInstance();
 
   inline UInt_t GetUint(std::string branchName) {
-    if (valuesTypes.find(branchName) != valuesTypes.end()) return valuesUint[branchName];
+    if (valuesTypes.find(branchName) != valuesTypes.end())
+      return valuesUint[branchName];
     return customValuesUint[branchName];
   }
   inline Int_t GetInt(std::string branchName) {
-    if (valuesTypes.find(branchName) != valuesTypes.end()) return valuesInt[branchName];
+    if (valuesTypes.find(branchName) != valuesTypes.end())
+      return valuesInt[branchName];
     return customValuesInt[branchName];
   }
   inline Bool_t GetBool(std::string branchName) {
-    if (valuesTypes.find(branchName) != valuesTypes.end()) return valuesBool[branchName];
+    if (valuesTypes.find(branchName) != valuesTypes.end())
+      return valuesBool[branchName];
     return customValuesBool[branchName];
   }
-  // inline Float_t GetFloat(std::string branchName) { return valuesFloat[branchName]; }
   inline Float_t GetFloat(std::string branchName) {
     if (valuesTypes.find(branchName) != valuesTypes.end())
       return valuesFloat[branchName];
     return customValuesFloat[branchName];
   }
   inline Double_t GetDouble(std::string branchName) {
-    if (valuesTypes.find(branchName) != valuesTypes.end()) return valuesDouble[branchName];
+    if (valuesTypes.find(branchName) != valuesTypes.end())
+      return valuesDouble[branchName];
     return customValuesDouble[branchName];
   }
   inline ULong64_t GetULong(std::string branchName) {
-    if (valuesTypes.find(branchName) != valuesTypes.end()) return valuesUlong[branchName];
+    if (valuesTypes.find(branchName) != valuesTypes.end())
+      return valuesUlong[branchName];
     return customValuesUlong[branchName];
   }
   inline UChar_t GetUChar(std::string branchName) {
-    if (valuesTypes.find(branchName) != valuesTypes.end()) return valuesUchar[branchName];
+    if (valuesTypes.find(branchName) != valuesTypes.end())
+      return valuesUchar[branchName];
     return customValuesUchar[branchName];
   }
-  inline Char_t GetChar(std::string branchName) { return valuesChar[branchName]; }
+  inline Char_t GetChar(std::string branchName) {
+    return valuesChar[branchName];
+  }
   inline UShort_t GetUShort(std::string branchName) {
-    if (valuesTypes.find(branchName) != valuesTypes.end()) return valuesUshort[branchName];
+    if (valuesTypes.find(branchName) != valuesTypes.end())
+      return valuesUshort[branchName];
     return customValuesUshort[branchName];
   }
   inline Short_t GetShort(std::string branchName) {
-    if (valuesTypes.find(branchName) != valuesTypes.end()) return valuesShort[branchName];
+    if (valuesTypes.find(branchName) != valuesTypes.end())
+      return valuesShort[branchName];
     return customValuesShort[branchName];
   }
 
-  std::map<std::string, std::string> valuesTypes;  /// contains all branch names and corresponding types
+  std::map<std::string, std::string>
+      valuesTypes; /// contains all branch names and corresponding types
   std::map<std::string, std::string> customValuesTypes;
   std::map<std::string, unsigned long> customValuesEpoch;
 
@@ -278,27 +324,29 @@ class Event {
   std::map<std::string, UShort_t[maxCollectionElements]> valuesUshortVector;
   std::map<std::string, Short_t[maxCollectionElements]> valuesShortVector;
 
-  std::map<std::string, std::vector<float>*> valuesStdFloatVector;
-  std::map<std::string, std::vector<double>*> valuesStdDoubleVector;
-  std::map<std::string, std::vector<int>*> valuesStdIntVector;
-  std::map<std::string, std::vector<unsigned int>*> valuesStdUintVector;
+  std::map<std::string, std::vector<float> *> valuesStdFloatVector;
+  std::map<std::string, std::vector<double> *> valuesStdDoubleVector;
+  std::map<std::string, std::vector<int> *> valuesStdIntVector;
+  std::map<std::string, std::vector<unsigned int> *> valuesStdUintVector;
 
   std::map<std::string, std::shared_ptr<PhysicsObjects>> collections;
   std::map<std::string, std::shared_ptr<PhysicsObjects>> extraCollections;
 
   bool hasExtraCollections = true;
-  insertion_ordered_map<std::string, ExtraCollection> extraCollectionsDescriptions;
+  insertion_ordered_map<std::string, ExtraCollection>
+      extraCollectionsDescriptions;
   std::map<std::string, std::string> defaultCollectionsTypes;
   std::map<std::string, std::pair<unsigned, unsigned>> runRangesPerEra;
 
   friend class EventReader;
-  template <typename T>
-  friend class Multitype;
+  template <typename T> friend class Multitype;
 
   template <typename First, typename... Rest>
-  bool tryGet(std::shared_ptr<PhysicsObject> physicsObject, std::string branchName, std::pair<float, float> cuts);
+  bool tryGet(std::shared_ptr<PhysicsObject> physicsObject,
+              std::string branchName, std::pair<float, float> cuts);
 
-  bool checkCuts(std::shared_ptr<PhysicsObject> physicsObject, std::string branchName, std::pair<float, float> cuts);
+  bool checkCuts(std::shared_ptr<PhysicsObject> physicsObject,
+                 std::string branchName, std::pair<float, float> cuts);
 };
 
 #endif /* Event_hpp */
